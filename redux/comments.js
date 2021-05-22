0fe7a7b8def36a89521252afdd92946305e15416
Randell Anderson <zugzug07@gmail.com>
2021-05-22 13:39:51 -0500

    Workshop Week 2

--- redux/comments.js
+++ redux/comments.js
@@ -5,6 +5,11 @@ export const comments = (state = { errMess: null, comments: []}, action) => {
         case ActionTypes.ADD_COMMENTS:
             return {...state, errMess: null, comments: action.payload};
 
+        case ActionTypes.ADD_COMMENT:
+            const comment = action.payload;
+            comment.id = state.comments.length;
+            return {...state, comments: state.comments.concat(comment)};
+
         case ActionTypes.COMMENTS_FAILED:
             return {...state, errMess: action.payload};
 
