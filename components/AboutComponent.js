ddd9e3766126342f235c5c3e3a4fbeaafa685c62
Randell Anderson <zugzug07@gmail.com>
2021-05-25 21:17:12 -0500

    Animatable

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -4,79 +4,84 @@ import { Card, ListItem } from "react-native-elements";
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl'; 
 import Loading from './LoadingComponent';
+import * as Animatable from 'react-native-animatable';
 
 const mapStateToProps = state => {
-  return {// use curly brackets here?
-    partners: state.partners
-  };
+	return {// use curly brackets here?
+		partners: state.partners
+	};
 };
 
 class About extends Component {
 
-  static navigationOptions = {
-    title: "About Us",
-  };
+	static navigationOptions = {
+		title: "About Us",
+	};
 
-  render() {
-    const renderPartner = ({ item }) => {
-      return (
-        <ListItem
-          title={item.name}
-          subtitle={item.description}
-          leftAvatar={{ source: {uri: baseUrl + item.image} }}
-        />
-      );
-    };
+	render() {
+		const renderPartner = ({ item }) => {
+			return (
+				<ListItem
+					title={item.name}
+					subtitle={item.description}
+					leftAvatar={{ source: {uri: baseUrl + item.image} }}
+				/>
+			);
+		};
 
-    if (this.props.partners.isLoading) {
-      return(
-        <ScrollView>
-          <Mission />
-          <Card title="Community Partners">
-            <Loading />
-          </Card>
-        </ScrollView>
-      );
-    }
-    if(this.props.partners.errMess) {
-      return(
-          <ScrollView>
-            <Mission />
-            <Card title="Community Partners">
-              <Text>{this.props.partners.errMess}</Text>
-            </Card>
-          </ScrollView>
-      );
-    }
-    return (
-      <ScrollView>
-        <Mission />
-        <Card title="Community Partners">
-          <FlatList
-            data={this.props.partners.partners}
-            renderItem={renderPartner}
-            keyExtractor={(item) => item.id.toString()}
-          />
-        </Card>
-      </ScrollView>
-    );
-  }
+		if (this.props.partners.isLoading) {
+			return(
+				<ScrollView>
+					<Mission />
+					<Card title="Community Partners">
+						<Loading />
+					</Card>
+				</ScrollView>
+			);
+		}
+		if(this.props.partners.errMess) {
+			return(
+				<ScrollView>
+					<Animatable.View animation='fadeInDown' duration={2000} delay={1000}>
+						<Mission />
+						<Card title="Community Partners">
+							<Text>{this.props.partners.errMess}</Text>
+						</Card>
+					</Animatable.View>
+				</ScrollView>
+			);
+		}
+		return (
+			<ScrollView>
+				<Animatable.View animation='fadeInDown' duration={2000} delay={1000}>
+					<Mission />
+					<Card title="Community Partners">
+						<FlatList
+							data={this.props.partners.partners}
+							renderItem={renderPartner}
+							keyExtractor={(item) => item.id.toString()}
+						/>
+					</Card>
+				</Animatable.View>
+			</ScrollView>
+		);
+	}
 }
 
 function Mission() {
-  return (
-    <Card title="Our Mission">
-      <Text style={{ margin: 10 }}>
-        We present a curated database of the best campsites in the vast woods
-        and backcountry of the World Wide Web Wilderness. We increase access to
-        adventure for the public while promoting safe and respectful use of
-        resources. The expert wilderness trekkers on our staff personally verify
-        each campsite to make sure that they are up to our standards. We also
-        present a platform for campers to share reviews on campsites they have
-        visited with each other.
-      </Text>
-    </Card>
-  );
+	return (
+		<Card title="Our Mission">
+			<Text style={{ margin: 10 }}>
+				We present a curated database of the best campsites in the vast woods
+				and backcountry of the World Wide Web Wilderness. We increase access to
+				adventure for the public while promoting safe and respectful use of
+				resources. The expert wilderness trekkers on our staff personally verify
+				each campsite to make sure that they are up to our standards. We also
+				present a platform for campers to share reviews on campsites they have
+				visited with each other.
+			</Text>
+		</Card>
+	);
 }
 
 export default connect(mapStateToProps)(About);
