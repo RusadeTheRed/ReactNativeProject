31ba0ae2e56c349a853fd175808ca18810ffab0a
Randell Anderson <zugzug07@gmail.com>
2021-06-01 12:51:59 -0500

    Sending Email

--- components/ReservationComponent.js
+++ components/ReservationComponent.js
@@ -99,12 +99,12 @@ class Reservation extends Component {
                             selectedValue={this.state.campers}
                             onValueChange={itemValue => this.setState({campers: itemValue})}
                         >
-                            <Picker.item label='1' value='1' />
-                            <Picker.item label='2' value='2' />
-                            <Picker.item label='3' value='3' />
-                            <Picker.item label='4' value='4' />
-                            <Picker.item label='5' value='5' />
-                            <Picker.item label='6' value='6' />
+                            <Picker.Item label='1' value='1' />
+                            <Picker.Item label='2' value='2' />
+                            <Picker.Item label='3' value='3' />
+                            <Picker.Item label='4' value='4' />
+                            <Picker.Item label='5' value='5' />
+                            <Picker.Item label='6' value='6' />
                         </Picker>
                     </View>
                     <View style={styles.formRow}>
